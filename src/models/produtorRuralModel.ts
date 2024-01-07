bf26bf68b55cc70dd00b556f5e869b4707048976
Bione <joao.bione@hotmail.com>
2024-01-06 21:08:08 -0300

    Database changes, creating INDEXES, inserting initital data in Cultura table.

--- src/models/produtorRuralModel.ts
+++ src/models/produtorRuralModel.ts
@@ -2,5 +2,16 @@ export interface ProdutorRural {
     id: number;
     cpf_cnpj: string;
     nome: string;
+    nomeFazenda: string;
+    cidade: string;
+    estado: string;
+    areaTotalHectares: number;
+    areaAgricultavelHectares: number;
+    areaVegetacaoHectares: number;
+    culturasPlantadas: Cultura[];
 }
 
+export interface Cultura {
+    id: number;
+    nome: string;
+}
